7bb2e422471b5f976faee7e182c54a5d1a1c67b3
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-10 22:32:36 +0300

    partner style added

--- src/Pages/Partners.jsx
+++ src/Pages/Partners.jsx
@@ -6,38 +6,38 @@ export default function Partners() {
       <div className="flex flex-col gap-10  ">
         {" "}
         <div className="text-5xl text-white z-40">Partners</div>
-        <div className="grid grid-cols-4 items-center justify-center gap-10 z-40 ">
-          <div className=" p-10 opacity-50  ">
+        <div className="grid grid-cols-4 items-center justify-center gap-10 z-50 ">
+          <div className="z-50 p-10 opacity-50 hover:opacity-100 duration-500 ">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-01.png"></img>
           </div>
-          <div className="z-50 p-10 opacity-50">
+          <div className="z-50 p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-02.png"></img>
           </div>
-          <div className="p-10 opacity-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-03.png"></img>
           </div>
-          <div className="p-10 opacity-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-04.png"></img>
           </div>
         </div>
-        <div className="grid grid-cols-4 items-center justify-center gap-10 z-40">
-          <div className="p-10 opacity-50">
+        <div className="grid grid-cols-4 items-center justify-center gap-10 z-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-05.png"></img>
           </div>
-          <div className="p-10 opacity-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-06.png"></img>
           </div>
-          <div className="p-10 opacity-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-07.png"></img>
           </div>
-          <div className="p-10 opacity-50">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
             {" "}
             <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-08.png"></img>
           </div>
