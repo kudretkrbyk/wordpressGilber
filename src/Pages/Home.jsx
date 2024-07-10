8b4529330a4ac04ae80b9120853fe889d1ae67aa
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-10 22:51:55 +0300

    Home page added style

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import { FaPlay } from "react-icons/fa";
+import { useRef } from "react";
 
 export default function Home() {
+  const [scale, setScale] = useState("scale-125");
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setScale("scale-100");
+          } else {
+            setScale("scale-125");
+          }
+        });
+      },
+      { threshold: 0.5 } // Görüntülenme oranı (0.5 = %50)
+    );
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    return () => {
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, []);
   return (
-    <div className="w-full h-screen relative  ">
-      <div className="z-0 absolute top-0 left-0 w-full">
-        <div className="bg-cover bg-center w-full h-screen bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/home.jpg)]   "></div>{" "}
+    <div className="w-full h-screen relative">
+      <div className="z-0 absolute top-0 left-0 w-full h-screen overflow-hidden">
+        <div
+          ref={imageRef}
+          className={`bg-cover bg-center w-full h-screen bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/home.jpg)] transition-transform duration-[4000ms] ${scale}`}
+        ></div>
       </div>
 
-      <div className="z-50 flex flex-col text-white w-6/12 h-screen  items-start justify-center border border-white p-48 gap-10">
-        <div className="z-50 relative flex items-center justify-center w-1/4 h-16 group ">
-          <div className=" absolute border p-10 rounded-full group-hover:scale-125 duration-500"></div>
+      <div className="z-50 flex flex-col text-white w-6/12 h-screen items-start justify-center border border-white p-48 gap-10">
+        <div className="z-50 relative flex items-center justify-center w-1/4 h-16 group">
+          <div className="absolute border p-10 rounded-full group-hover:scale-125 duration-500"></div>
           <div className="absolute">
-            {" "}
             <FaPlay className="size-10 group-hover:scale-110 duration-500" />
           </div>
         </div>
@@ -20,7 +51,7 @@ export default function Home() {
           <div className="text-4xl">Gilber</div>
           <div className="text-5xl text-red-500">.</div>
         </div>
-        <div className="z-50 ">
+        <div className="z-50">
           Working with client and community, we deliver masterplans that create
           vibrant new places and spaces, attract people, and encourage.
         </div>
