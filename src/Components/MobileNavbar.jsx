157821b2cc6fafd38dbac5f3420638c8b56d079a
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-11 21:58:47 +0300

    added mobileMenu

---
+++ src/Components/MobileNavbar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-scroll";
+import { RiCloseLargeFill } from "react-icons/ri";
+
+export default function MobileNavbar() {
+  return (
+    <div className=" flex flex-col w-full  h-screen items-center justify-center p-10 text-white fixed z-50 bg-[#161616] ">
+      <div className="z-50 flex items-center justify-end w-full">
+        <RiCloseLargeFill className="size-10" />
+      </div>
+
+      <div className="z-50 flex items-center justify-center gap-16 w-full">
+        <div className="flex  flex-col items-center justify-center gap-10 w-full">
+          <div className="flex items-end justify-center gap-1">
+            <div className="text-4xl">Gilber </div>
+            <div className="text-4xl text-red-700">.</div>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="home" smooth={true}>
+              HOME
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="about" smooth={true}>
+              ABOUT
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="projects" smooth={true}>
+              PROJECTS
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="education" smooth={true}>
+              EDUCATION
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="testimonials" smooth={true}>
+              TESTIMONIALS
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="partners" smooth={true}>
+              PARTNERS
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+            <Link to="blog" smooth={true}>
+              BLOG
+            </Link>
+          </div>
+          <div className="hover:text-red-500 duration-500 bg-red-500 hover:cursor-pointer">
+            <Link to="contact" smooth={true}>
+              CONTACT
+            </Link>
+          </div>
+          <div>+7 (212) 674-25-10</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,58 +1,65 @@
 import { Link } from "react-scroll";
+import MobileNavbar from "./MobileNavbar";
 
 export default function Navbar() {
   return (
-    <div className="flex w-full items-center justify-between p-10 text-white fixed z-50 ">
-      <div className="flex items-center justify-center gap-16 w-full">
-        <div className="flex items-end justify-center gap-1">
-          <div className="text-4xl">Gilber</div>
-          <div className="text-4xl text-red-700">.</div>
+    <div className="flex w-full items-center justify-between text-white fixed z-50 ">
+      <div className="hidden navbar  items-center justify-between w-full p-10">
+        {" "}
+        <div className="flex items-center justify-center gap-16 w-full">
+          <div className="flex items-end justify-center gap-1">
+            <div className="text-4xl">Gilber</div>
+            <div className="text-4xl text-red-700">.</div>
+          </div>
+          <div className="flex items-center justify-center gap-10 w-full">
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="home" smooth={true}>
+                HOME
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="about" smooth={true}>
+                ABOUT
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="projects" smooth={true}>
+                PROJECTS
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="education" smooth={true}>
+                EDUCATION
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="testimonials" smooth={true}>
+                TESTIMONIALS
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="partners" smooth={true}>
+                PARTNERS
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
+              <Link to="blog" smooth={true}>
+                BLOG
+              </Link>
+            </div>
+            <div className="hover:text-red-500 duration-500 bg-red-500 hover:cursor-pointer">
+              <Link to="contact" smooth={true}>
+                CONTACT
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-10 w-full">
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="home" smooth={true}>
-              HOME
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="about" smooth={true}>
-              ABOUT
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="projects" smooth={true}>
-              PROJECTS
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="education" smooth={true}>
-              EDUCATION
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="testimonials" smooth={true}>
-              TESTIMONIALS
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="partners" smooth={true}>
-              PARTNERS
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 hover:cursor-pointer">
-            <Link to="blog" smooth={true}>
-              BLOG
-            </Link>
-          </div>
-          <div className="hover:text-red-500 duration-500 bg-red-500 hover:cursor-pointer">
-            <Link to="contact" smooth={true}>
-              CONTACT
-            </Link>
-          </div>
+        <div className="flex items-center justify-end text-nowrap w-full">
+          +7 (212) 674-25-10
         </div>
       </div>
-      <div className="flex items-center justify-end text-nowrap w-full">
-        +7 (212) 674-25-10
+      <div className="z-50 mobile-navbar">
+        <MobileNavbar></MobileNavbar>
       </div>
     </div>
   );
