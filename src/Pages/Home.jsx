4219780c9416aea91ce8dd2b7bbe13536fd9f527
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-11 22:50:10 +0300

    added responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -35,11 +35,11 @@ export default function Home() {
       <div className="z-0 absolute top-0 left-0 w-full h-screen overflow-hidden">
         <div
           ref={imageRef}
-          className={`bg-cover bg-center w-full h-screen bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/home.jpg)] transition-transform duration-[4000ms] ${scale}`}
+          className={`bg-cover bg-center w-full h-screen bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/home-mobile-640x1080.jpg)] md:bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/home.jpg)] transition-transform duration-[4000ms] ${scale}`}
         ></div>
       </div>
 
-      <div className="z-40 flex flex-col text-white w-full md:w-8/12 xl:w-6/12 h-screen md:items-start items-center justify-center border border-white p-24 md:p-32 xl:p-48 gap-10">
+      <div className="z-40 flex flex-col text-white w-full md:w-8/12 xl:w-6/12 h-screen items-start  justify-center border border-white p-10 md:p-32 xl:p-48 gap-10">
         <div className="z-40 relative flex items-center justify-center w-1/4 h-16 group">
           <div className="absolute border p-10 rounded-full group-hover:scale-125 duration-500"></div>
           <div className="absolute">
@@ -48,7 +48,7 @@ export default function Home() {
         </div>
         <div className="z-40">Product Designer</div>
         <div className="flex items-end gap-2 z-40 text-xl">
-          <div className="text-4xl">Gilber</div>
+          <div className="text-5xl">Gilber</div>
           <div className="text-5xl text-red-500">.</div>
         </div>
         <div className="z-40">
