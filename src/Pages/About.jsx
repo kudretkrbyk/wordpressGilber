794a132d538c52e9565412827d7a4e01c1096a59
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-11 23:15:24 +0300

    added responsive

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -38,73 +38,77 @@ export default function About() {
   }, []);
 
   return (
-    <div className="flex items-center justify-around px-40 w-full h-screen bg-black ">
-      <div className="flex flex-col items-center gap-4 justify-center border-l border-b border-t border-white w-3/12 h-4/6">
-        <div className="flex z-40 items-end justify-center text-white w-full">
-          <div className=" text-9xl ">9</div>
-          <div className="text-8xl text-red-500">.</div>
-        </div>
-        <div className="flex w-full items-end justify-center gap-5 text-white p-10">
-          <div className="w-full h-[1px] bg-white"></div>
-          <div className="w-full text-wrap text-2xl text-right">
-            Years Experience Working
+    <div className="flex flex-col xl:flex-row items-center justify-around   w-full xl:h-screen h-full bg-black gap-14 p-20 ">
+      <div className=" w-full h-[500px] flex items-center justify-start px-20   ">
+        <div className="flex flex-col items-center gap-4 justify-center border-l border-b border-t border-white w-[340px] h-full">
+          <div className="flex z-40 items-end justify-center text-white w-full">
+            <div className=" text-9xl ">9</div>
+            <div className="text-8xl text-red-500">.</div>
+          </div>
+          <div className="flex w-full items-end justify-center gap-5 text-white p-10">
+            <div className="w-full h-[1px] bg-white"></div>
+            <div className="w-full text-wrap text-2xl text-right">
+              Years Experience Working
+            </div>
           </div>
         </div>
       </div>
-      <div className=" flex flex-col items-start justify-center gap-10 p-5  w-5/12  h-4/6">
-        <div className="text-white text-2xl font-bold">Great Experience</div>
-        <div className="text-white text-2xl">
-          Fill appear won’t may make moveth signs. Fourth. Good own. Green
-          you’re moveth us, lesser.
-        </div>
-        <div className="flex flex-col gap-16 w-full">
-          {" "}
-          <div
-            ref={skilBarsRef}
-            className="w-full  text-white flex flex-col gap-3"
-          >
-            <div className="flex items-center justify-between">
-              <div>WEB DESIGN</div>
-              <div className="web-design"> {webDesignWidth}%</div>
-            </div>
-            <div className="w-full relative ">
-              <div
-                className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
-                style={{ width: `${webDesignWidthObserver}%` }}
-              ></div>
-              <div className=" absolute w-full h-2 bg-white rounded-full"></div>
-            </div>
+      <div className=" flex px-20    w-full  h-[500px]">
+        <div className="flex flex-col gap-10 items-start justify-center w-full h-full ">
+          <div className="text-white text-2xl font-bold">Great Experience</div>
+          <div className="text-white text-2xl">
+            Fill appear won’t may make moveth signs. Fourth. Good own. Green
+            you’re moveth us, lesser.
           </div>
-          <div
-            ref={skilBarsRef}
-            className="w-full text-white flex flex-col gap-3"
-          >
-            <div className="flex items-center justify-between">
-              <div>FIGMA</div>
-              <div className="figma">{figmaWidth}%</div>
+          <div className="flex flex-col gap-16 w-full">
+            {" "}
+            <div
+              ref={skilBarsRef}
+              className="w-full  text-white flex flex-col gap-3"
+            >
+              <div className="flex items-center justify-between">
+                <div>WEB DESIGN</div>
+                <div className="web-design"> {webDesignWidth}%</div>
+              </div>
+              <div className="w-full relative ">
+                <div
+                  className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
+                  style={{ width: `${webDesignWidthObserver}%` }}
+                ></div>
+                <div className=" absolute w-full h-2 bg-white rounded-full"></div>
+              </div>
             </div>
-            <div className="w-full relative ">
-              <div
-                className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
-                style={{ width: `${figmaWidthObserver}%` }}
-              ></div>
-              <div className=" absolute w-full h-2 bg-white rounded-full"></div>
+            <div
+              ref={skilBarsRef}
+              className="w-full text-white flex flex-col gap-3"
+            >
+              <div className="flex items-center justify-between">
+                <div>FIGMA</div>
+                <div className="figma">{figmaWidth}%</div>
+              </div>
+              <div className="w-full relative ">
+                <div
+                  className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
+                  style={{ width: `${figmaWidthObserver}%` }}
+                ></div>
+                <div className=" absolute w-full h-2 bg-white rounded-full"></div>
+              </div>
             </div>
-          </div>
-          <div
-            ref={skilBarsRef}
-            className="w-full text-white flex flex-col gap-3"
-          >
-            <div className="flex items-center justify-between">
-              <div>WORDPRESS</div>
-              <div className="wordpress"> {wordpressWidth} %</div>
-            </div>
-            <div className="w-full relative ">
-              <div
-                className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
-                style={{ width: `${wordpressWidthObserver}%` }}
-              ></div>
-              <div className=" absolute w-full h-2 bg-white rounded-full"></div>
+            <div
+              ref={skilBarsRef}
+              className="w-full text-white flex flex-col gap-3"
+            >
+              <div className="flex items-center justify-between">
+                <div>WORDPRESS</div>
+                <div className="wordpress"> {wordpressWidth} %</div>
+              </div>
+              <div className="w-full relative ">
+                <div
+                  className="absolute z-40  h-2 bg-red-500 rounded-full duration-[4000ms]"
+                  style={{ width: `${wordpressWidthObserver}%` }}
+                ></div>
+                <div className=" absolute w-full h-2 bg-white rounded-full"></div>
+              </div>
             </div>
           </div>
         </div>
