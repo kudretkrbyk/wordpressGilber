d230bc6f362b77eaf80d815c8eb526221ca18e4e
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-12 18:54:41 +0300

    added responsive

--- src/Pages/Partners.jsx
+++ src/Pages/Partners.jsx
@@ -1,42 +1,38 @@
 export default function Partners() {
   return (
-    <div className="w-full h-screen flex items-center justify-center  relative ">
-      <div className="w-full h-screen bg-[#161616] absolute z-40 "></div>
+    <div className="w-full md:h-screen flex items-center justify-center  relative p-1 md:p-20 ">
+      <div className="w-full h-full bg-[#161616] absolute z-30 "></div>
 
-      <div className="flex flex-col gap-10  ">
+      <div className="flex flex-col items-start justify-center gap-10 w-full h-full p-20  ">
         {" "}
-        <div className="text-5xl text-white z-40">Partners</div>
-        <div className="grid grid-cols-4 items-center justify-center gap-10 z-40 ">
-          <div className="z-40 p-10 opacity-50 hover:opacity-100 duration-500 ">
-            {" "}
-            <img src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-01.png"></img>
+        <div className="text-5xl text-white z-40  w-full">Partners</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center justify-center place-items-center gap-1 md:gap-10 z-40 w-full   ">
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500 self-center  ">
+            <img
+              src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-01.png"
+              alt="Partner 1"
+            />
           </div>
-          <div className="p-10 opacity-50 hover:opacity-100 duration-500 z-20 relative">
-            <div className="z-20 relative">
-              <img
-                src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-02.png"
-                alt="Partner 2"
-              />
-            </div>
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
+            <img
+              src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-02.png"
+              alt="Partner 2"
+            />
           </div>
-          <div className="p-10 opacity-50 hover:opacity-100 duration-500 z-20 relative">
-            <div className="z-20 relative">
-              <img
-                src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-03.png"
-                alt="Partner 3"
-              />
-            </div>
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
+            <img
+              src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-03.png"
+              alt="Partner 3"
+            />
           </div>
-          <div className="p-10 opacity-50 hover:opacity-100 duration-500 z-20 relative">
-            <div className="z-20 relative">
-              <img
-                src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-04.png"
-                alt="Partner 4"
-              />
-            </div>
+          <div className="p-10 opacity-50 hover:opacity-100 duration-500">
+            <img
+              src="http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/logo-04.png"
+              alt="Partner 4"
+            />
           </div>
         </div>
-        <div className="grid grid-cols-4 items-center justify-center gap-10 z-40">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center place-items-center gap-1 md:gap-10 z-40 w-full ">
           <div className="p-10 opacity-50 hover:opacity-100 duration-500 z-20 relative">
             <div className="z-20 relative">
               <img
